9c28368b144d57af22719dd0779f29f6d5291bf6
B20-Shahzod <varzob33@gmail.com>
2020-10-31 12:46:09 -0400

    group 27 is the best

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,4 +1,10 @@
 public class HalloweenTest {
 
     // group 27 is the best
+    // going to the market soon
+    // early birds ? good question
+    // most likely - yes
+
+    // do you like Java ?
+    // yes, of course
 }
